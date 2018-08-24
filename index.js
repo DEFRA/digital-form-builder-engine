eca206d32a3f451f115ee4b71761e6a5a6444856
Stone, David <david.stone@environment-agency.gov.uk>
2018-08-24 21:42:30 +0100

    Playground mode

--- index.js
+++ index.js
@@ -13,73 +13,129 @@ module.exports = {
     version: pkg.version,
     dependencies: 'vision',
     register: (server, options) => {
-      const { data, getState, mergeState, ordnanceSurveyKey } = options
+      const { data, getState, mergeState, ordnanceSurveyKey, playgroundModel } = options
 
-      const model = makeModel(data)
+      const playgroundMode = !data
+      const model = !playgroundMode && makeModel(data)
 
-      model.pages.forEach(page => {
-        if (page.path === '/TESTXXX') {
-          const options = require('../govuk-site/server/routes/custom.js')
-          const defaults = {
-            path: page.path,
-            options: {
-              plugins: {
-                [pkg.name]: { page }
-              }
-            }
-          }
-
-          // Clone the required route option(s)
-          // and apply the default properties.
-          const routes = [].concat(hoek.clone(options))
-            .map(route => hoek.merge(route, defaults))
-
-          // Register the route(s)
-          server.route(routes)
+      const getModel = (request) => {
+        if (!playgroundMode) {
+          return model
         } else {
-          // GET
-          server.route({
-            method: 'get',
-            path: page.path,
-            handler: async (request, h) => {
-              const state = await getState(request)
-              const formData = page.getFormDataFromState(state)
+          return makeModel(request.yar.get('model') || playgroundModel)
+        }
+      }
 
-              return h.view('index', page.getViewModel(formData))
-            }
-          })
+      async function get (request, page, h) {
+        const state = await getState(request)
+        const formData = page.getFormDataFromState(state)
+        return h.view('index', page.getViewModel(formData))
+      }
 
-          // POST
-          server.route({
-            method: 'post',
-            path: page.path,
-            handler: async (request, h) => {
-              const payload = request.payload
-              const options = { abortEarly: false }
-              const formResult = joi.validate(payload, page.formSchema, options)
+      async function post (request, page, h) {
+        const payload = request.payload
+        const options = { abortEarly: false }
+        const formResult = joi.validate(payload, page.formSchema, options)
 
-              if (formResult.error) {
-                return h.view('index', page.getViewModel(payload, formResult))
-              } else {
-                const newState = page.getStateFromValidForm(formResult.value)
-                const stateResult = joi.validate(newState, page.stateSchema, options)
+        if (formResult.error) {
+          return h.view('index', page.getViewModel(payload, formResult))
+        } else {
+          const newState = page.getStateFromValidForm(formResult.value)
+          const stateResult = joi.validate(newState, page.stateSchema, options)
 
-                if (stateResult.error) {
-                  return h.view('index', page.getViewModel(payload, stateResult))
-                } else {
-                  const update = page.section ? {
-                    [page.section.name]: stateResult.value
-                  } : stateResult.value
+          if (stateResult.error) {
+            return h.view('index', page.getViewModel(payload, stateResult))
+          } else {
+            const update = page.section ? {
+              [page.section.name]: stateResult.value
+            } : stateResult.value
 
-                  const state = await mergeState(request, update)
+            const state = await mergeState(request, update)
 
-                  return proceed(request, h, page.getNext(state))
+            return proceed(request, h, page.getNext(state))
+          }
+        }
+      }
+
+      if (!playgroundMode) {
+        model.pages.forEach(page => {
+          if (page.path === '/TESTXXX') {
+            const options = require('../govuk-site/server/routes/custom.js')
+            const defaults = {
+              path: page.path,
+              options: {
+                plugins: {
+                  [pkg.name]: { page }
                 }
               }
             }
+
+            // Clone the required route option(s)
+            // and apply the default properties.
+            const routes = [].concat(hoek.clone(options))
+              .map(route => hoek.merge(route, defaults))
+
+            // Register the route(s)
+            server.route(routes)
+          } else {
+            // GET
+            server.route({
+              method: 'get',
+              path: page.path,
+              handler: (request, h) => {
+                return get(request, page, h)
+              }
+            })
+
+            // POST
+            server.route({
+              method: 'post',
+              path: page.path,
+              handler: async (request, h) => {
+                return post(request, page, h)
+              }
+            })
+          }
+        })
+      } else {
+        const findPage = (model, request) => {
+          return model.pages.find(page => {
+            return page.path === '/' + (request.params.path || '')
           })
         }
-      })
+
+        // GET PLAYGROUND
+        server.route({
+          method: 'get',
+          path: '/{path*}',
+          handler: async (request, h) => {
+            const model = getModel(request)
+            const page = findPage(model, request)
+
+            if (!page) {
+              return boom.notFound(page.path)
+            }
+
+            return get(request, page, h)
+          }
+        })
+
+        // POST PLAYGROUND
+        server.route({
+          method: 'post',
+          path: '/{path*}',
+          handler: async (request, h) => {
+            const model = getModel(request)
+            const page = findPage(model, request)
+
+            if (!page) {
+              return boom.notFound(page.path)
+            }
+
+            return post(request, page, h)
+          }
+        })
+      }
 
       // SUMMARY
       server.route({
@@ -87,7 +143,7 @@ module.exports = {
         path: '/summary',
         handler: async (request, h) => {
           const state = await getState(request)
-          const viewModel = new SummaryViewModel(model, state)
+          const viewModel = new SummaryViewModel(getModel(request), state)
 
           return h.view('summary', viewModel)
         }
