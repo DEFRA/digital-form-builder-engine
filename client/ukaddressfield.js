229adb3b881d71a4498e4b8f34ac0120350c3ea0
David Stone <davidjamesstone@gmail.com>
2018-09-11 18:07:21 +0100

    Remove jQuery

--- client/ukaddressfield.js
+++ client/ukaddressfield.js
@@ -1,81 +1,120 @@
-/* global $ */
+var $components = document.querySelectorAll('.uk-address-component')
 
-$(function () {
-  var $components = $('.uk-address-component')
-  var $forms = $components.parents('form')
+$components.forEach(function (component) {
+  var form = component.parentNode.parentNode
+  var lookup = component.querySelector('.uk-address-lookup')
+  var manual = component.querySelector('.uk-address-manual')
+  var submitButton = form.querySelector('button[type=submit]')
+  var lookupQuery = form.querySelector('.uk-address-query')
+  var lookupSelector = form.querySelector('.uk-address-selector')
+  var postcode = lookupQuery.querySelector('input.postcode-query')
+  var lookupButton = lookupQuery.querySelector('button.postcode-lookup')
+  var postcodeDisplay = lookupSelector.querySelector('.postcode-query-display')
+  var selector = lookupSelector.querySelector('.uk-address-selector select')
+  var lookupLinks = form.querySelectorAll('.postcode-query-link')
+  var manualLink = form.querySelector('.postcode-manual-link')
 
-  $components.each(function (index, component) {
-    if ($(component).find('.uk-address-manual .govuk-form-group--error').length === 0) {
-      showLookup($(component))
-    } else {
-      showManual($(component))
-    }
-  })
+  if (!component.querySelector('.uk-address-manual .govuk-form-group--error')) {
+    showLookup()
+  } else {
+    showManual()
+  }
 
-  $forms.on('click', 'button.postcode-lookup', function (e) {
+  lookupButton.addEventListener('click', function (e) {
     e.preventDefault()
-    var $btn = $(this)
-    var $postcode = $btn.prev()
-    var $component = $btn.closest('.uk-address-component')
-    var $select = $('.uk-address-selector select', $component)
-    var postcodeQuery = $postcode.val().trim().toUpperCase()
+    var postcodeQuery = postcode.value.trim().toUpperCase()
 
     if (!postcodeQuery) {
-      return showManual($component)
+      return showManual()
     }
 
-    $.getJSON('/find-address?postcode=' + postcodeQuery, function (results) {
-      var label = `${results.length} Address${results.length > 1 ? 'es' : ''} found`
-      $select.html(`<option>${label}</option>` + results.map(result => (
-        `<option value="${result.uprn}">${result.address}</option>`
-      )).join(''))
-      $('.postcode-query-display', $component).text(postcodeQuery)
-      $component.closest('form').find('button[type=submit]').show()
-      $component.find('.uk-address-query').hide()
-      $component.find('.uk-address-selector').show()
-      $select.data('results', results)
-    }).fail(function () {
-      showManual($component)
+    getJSON('/find-address?postcode=' + postcodeQuery, function (err, results) {
+      if (err) {
+        showManual()
+        return
+      }
+
+      var label = results.length + ' Address' + (results.length > 1 ? 'es' : '') + ' found'
+
+      var options = '<option>' + label + '</option>'
+
+      results.forEach(function (result) {
+        options += '<option value="' + result.uprn + '">' + result.address + '</option>'
+      })
+
+      selector.innerHTML = options
+
+      postcodeDisplay.textContent = postcodeQuery
+      submitButton.style.display = ''
+      lookupQuery.style.display = 'none'
+      lookupSelector.style.display = ''
+      selector._results = results
     })
   })
 
-  $forms.on('change', '.uk-address-selector select', function () {
-    var $select = $(this)
-    var results = $select.data('results')
-    var value = $select.val()
+  selector.addEventListener('change', function (e) {
+    var results = selector._results
+    var value = selector.value
     var result = results.find(result => result.uprn === value).item
-    console.log(result)
-    var $form = $select.closest('form')
-    $form.find('[name$="premises"]').val(result.BUILDING_NUMBER || result.BUILDING_NAME)
-    $form.find('[name$="street"]').val(result.THOROUGHFARE_NAME)
-    $form.find('[name$="locality"]').val(result.DEPENDENT_LOCALITY)
-    $form.find('[name$="town"]').val(result.POST_TOWN)
-    $form.find('[name$="postcode"]').val(result.POSTCODE)
+    // console.log(result)
+    form.querySelector('[name$="premises"]').value = result.BUILDING_NUMBER || result.BUILDING_NAME || ''
+    form.querySelector('[name$="street"]').value = result.THOROUGHFARE_NAME || ''
+    form.querySelector('[name$="locality"]').value = result.DEPENDENT_LOCALITY || ''
+    form.querySelector('[name$="town"]').value = result.POST_TOWN || ''
+    form.querySelector('[name$="postcode"]').value = result.POSTCODE || ''
   })
 
-  $forms.on('click', '.postcode-query-link', function (e) {
+  lookupLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault()
+      showLookup()
+      lookupQuery.style.display = ''
+      lookupSelector.style.display = 'none'
+    })
+  })
+
+  manualLink.addEventListener('click', function (e) {
     e.preventDefault()
-    var $component = $(this).closest('.uk-address-component')
-    showLookup($component)
-    $component.find('.uk-address-query').show()
-    $component.find('.uk-address-selector').hide()
+    showManual()
   })
 
-  $forms.on('click', '.postcode-manual-link', function (e) {
-    e.preventDefault()
-    var $component = $(this).closest('.uk-address-component')
-    showManual($component)
-  })
-
-  function showManual ($component) {
-    $component.find('.uk-address-lookup').hide()
-    $component.find('.uk-address-manual').show()
-    $component.parents('form').find('button[type=submit]').show()
+  function showManual () {
+    lookup.style.display = 'none'
+    manual.style.display = ''
+    submitButton.style.display = ''
   }
 
-  function showLookup ($component) {
-    $component.find('.uk-address-lookup').show()
-    $component.find('.uk-address-manual').hide()
-    $component.parents('form').find('button[type=submit]').hide()
+  function showLookup () {
+    lookup.style.display = ''
+    manual.style.display = 'none'
+    submitButton.style.display = 'none'
   }
 })
+
+function getJSON (url, callback) {
+  var request = new window.XMLHttpRequest()
+  request.open('GET', url, true)
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      try {
+        var data = JSON.parse(request.responseText)
+      } catch (err) {
+        callback(err)
+        return
+      }
+
+      callback(null, data)
+    } else {
+      // It returned an error code
+      callback(request.status)
+    }
+  }
+
+  request.onerror = function (err) {
+    callback(err)
+  }
+
+  request.send()
+}
